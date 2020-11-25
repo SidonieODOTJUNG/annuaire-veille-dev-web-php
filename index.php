9fe8d6eb8ab37b4a3b0b368d67147df80468d981
SidonieODOTJUNG <odot.sidonie@hotmail.fr>
2020-11-25 15:28:46 +0100

    Première version html+array php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- tp veille techno -->
+
+<!-- partie 1 -->
+
+<?php
+    $listemploi = 
+    [
+        [
+            'titre'=> 'Développeur : le joker des professionnels en transition',
+            'date'=> '18/11/2020',
+            'site' => 'Silicon.fr' ,
+            'contenu'=> 'Analyse des débouchés professionnels suite à une reconversion en dev web...'
+        ],
+
+        [
+            'titre'=> 'Compétences JS les plus recherchées',
+            'date'=> '23/12/2019',
+            'site' => 'ZDNet',
+            'contenu'=> 'Choix des bibliothèques et frameworks JavaScript, en fonction des offres, en décembre 2019.' 
+        ],
+
+        [
+            'titre'=>'Investissements IT : un retour à la croissance en 2021',
+            'date'=> '13/11/2020',
+            'site' => 'Silicon.fr' ,
+            'contenu'=> 'Analyse des secteurs IT concernés par la reprise des investissements...' 
+        ],
+
+    ];
+?>
+
+<!-- 2nd partie -->
