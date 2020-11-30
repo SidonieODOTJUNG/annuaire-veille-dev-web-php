a309df076485b64ddab085b7e421106d626fd9fc
SidonieODOTJUNG <odot.sidonie@hotmail.fr>
2020-11-30 08:33:58 +0100

    finalisation modèle page article

--- ajoutArt.php
+++ ajoutArt.php
@@ -11,10 +11,16 @@
                 <input type="url" name="url" id="url"> <br>
                 <label for="date">Entrer la date de l'article</label> <br>
                 <input type="date" name="date" id="date"> <br>
+                <label for="theme">Choissir le thème de l'article</label><br/>
+                <select name="theme" id="theme">
+                    <option value="innov">Innovation</option>
+                    <option value="techDevWeb">Tech Dev Web</option>
+                    <option value="techRH">Digitalisation RH</option>
+                    <option value="emploi">Marché de l'emploi Dev Web</option>
+                </select> <br>
                 <label for="resume">Entrer un résumé</label> <br>
                 <textarea name="resume" id="resume" cols="30" rows="10"></textarea> <br>
                 <input type="submit" name="ajoutArt" id="ajoutArt" value="Ajouter l'article" >
             </fieldset>
-            
         </form>
     </section>        
